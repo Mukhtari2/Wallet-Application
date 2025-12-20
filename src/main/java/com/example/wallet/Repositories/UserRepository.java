987a89a153d053d98f1ca61c6827c520e20faf5f
BARHAMA <suleimannmukhtar@gmail.com>
2025-12-20 07:58:42 +0100

    progress

--- src/main/java/com/example/wallet/Repositories/UserRepository.java
+++ src/main/java/com/example/wallet/Repositories/UserRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository
         extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findUserById(Long id);
 
     boolean existsByEmail(String email);
 }
