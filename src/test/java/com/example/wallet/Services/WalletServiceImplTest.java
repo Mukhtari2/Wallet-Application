5cb0480830dcc164547997b76d8decd4c966ee0f
BARHAMA <suleimannmukhtar@gmail.com>
2025-12-28 21:46:42 +0100

    progress

--- src/test/java/com/example/wallet/Services/WalletServiceImplTest.java
+++ src/test/java/com/example/wallet/Services/WalletServiceImplTest.java
@@ -44,54 +44,60 @@ class WalletServiceImplTest {
     @Test
     void testToCreateNewWalletForUser() {
             User user = new User();
+            user.setName("Abu");
             user.setEmail("ZbbyMicheal23@gmail.com");
-            user.setName("Zubby") ;
 
+            assertEquals(0, userRepository.findAll().size());
+            User newUser = userRepository.save(user);
+            assertEquals(1, userRepository.findAll().size());
 
             WalletDTO walletDTO = new WalletDTO();
-            walletDTO.setName("Binance" );
-            walletDTO.setUserId(user.getId());
+            walletDTO.setName("wallet 1 " + newUser.getName());
+            walletDTO.setUserId(newUser.getId());
             walletDTO.setBalance(new BigDecimal("300.00"));
 
+            assertEquals(0, walletRepository.findAll().size());
             WalletDTO newWallet = walletService.createNewWalletForUser(user);
+            assertEquals(1, walletRepository.findAll().size());
 
             assertNotNull(walletDTO);
-            assertEquals("Binance", newWallet.getName());
+            assertEquals("wallet 1 " + newUser.getName(), newWallet.getName());
             assertEquals(user.getId(), newWallet.getUserId());
-            assertEquals("Zubby", user.getName());
+            assertEquals("Abu", user.getName());
     }
 
-//    @Test
-//    void testToViewAllWalletCreated(){
-//        UserDTO user1 = new UserDTO();
-//        user1.setEmail("ZubbyMicheal23@gmail.com");
-//        user1.setName("Zubby") ;
-//        UserDTO newUser = userService.createNewUser(user1);
-//
-//        WalletDTO wallet1   = new WalletDTO();
-//        wallet1.setName("Nino wallet");
-//        wallet1.setBalance(new BigDecimal(9000.00));
-//        wallet1.setUserId(newUser.getId());
-//
-//        UserDTO user2 = new UserDTO();
-//        user2.setEmail("MusaDanladi21@gmail.com");
-//        user2.setName("Musa") ;
-//        UserDTO newUser2 = userService.createNewUser(user2);
-//
-//        WalletDTO wallet2 = new WalletDTO();
-//        wallet2.setName("Binance");
-//        wallet2.setBalance(new BigDecimal(45000.00));
-//        wallet2.setUserId(newUser2.getId());
-//
-//       walletServiceImpl.saveAllWallets(List.of(wallet1, wallet2));
-//
-//        List<WalletDTO> savedWallets = walletServiceImpl.listAllWalletForUser();
-//
-//        assertNotNull(savedWallets);
-//        assertEquals(2, savedWallets.size());
-//        assertEquals("Nino wallet", savedWallets.getFirst().getName());
-//        assertEquals("musadanladi21@gmail.com", user2.getEmail());
-//        assertEquals(new BigDecimal("9000.00"), savedWallets.getFirst().getBalance());
-//
-//    }
+    @Test
+    void testToViewAllWalletCreated(){
+        User user = new User();
+        user.setName("Abu");
+        user.setEmail("ZbbyMicheal23@gmail.com");
+        User user1 = userRepository.save(user);
+
+
+        WalletDTO wallet1 = new WalletDTO();
+        wallet1.setName("Nino wallet");
+        wallet1.setBalance(new BigDecimal(9000.00));
+        wallet1.setUserId(newUser.getId());
+
+        UserDTO user2 = new UserDTO();
+        user2.setEmail("MusaDanladi21@gmail.com");
+        user2.setName("Musa") ;
+        UserDTO newUser2 = userService.createNewUser(user2);
+
+        WalletDTO wallet2 = new WalletDTO();
+        wallet2.setName("Binance");
+        wallet2.setBalance(new BigDecimal(45000.00));
+        wallet2.setUserId(newUser2.getId());
+
+       walletServiceImpl.saveAllWallets(List.of(wallet1, wallet2));
+
+        List<WalletDTO> savedWallets = walletServiceImpl.listAllWalletForUser();
+
+        assertNotNull(savedWallets);
+        assertEquals(2, savedWallets.size());
+        assertEquals("Nino wallet", savedWallets.getFirst().getName());
+        assertEquals("musadanladi21@gmail.com", user2.getEmail());
+        assertEquals(new BigDecimal("9000.00"), savedWallets.getFirst().getBalance());
+
+    }
 }
