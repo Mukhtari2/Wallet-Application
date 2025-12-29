7384fcadc31eed58c6ddc86582b4a3a10f5c2c39
BARHAMA <suleimannmukhtar@gmail.com>
2025-12-29 10:39:49 +0100

    implemented the environment variable

--- src/main/java/com/example/wallet/Services/UserServiceImpl.java
+++ src/main/java/com/example/wallet/Services/UserServiceImpl.java
@@ -26,6 +26,7 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
+
     @Transactional
     public UserDTO createNewUser(UserDTO userDTO) {
         if (userRepository.existsByEmail(userDTO.getEmail())) {
